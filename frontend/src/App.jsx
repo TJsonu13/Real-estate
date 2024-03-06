9b496982adb08f5eed45a3b36480abf3ed8c189d
tanmay <tanmayjindal651@gmail.com>
2024-03-06 23:25:51 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home"
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 export default function App() {
-  return (
-    <div className='text-red-500'>
-      App
-      
-    </div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path ="/" element = {<Home></Home>}></Route>
+    <Route path ="/sign-in" element = {<Signin></Signin>}></Route>
+    <Route path ="/sign-up" element = {<SignUp></SignUp>}></Route>
+    <Route path ="/about" element = {<About></About>}></Route>
+    <Route path ="/profile" element = {<Profile></Profile>}></Route>
+  </Routes>
+  </BrowserRouter>
 }
